b1638ee6aea6b41bca08d21952f2729dbe09d8e7
Matt Greenfield <gmattgreenfield@gmail.com>
2014-11-14 08:10:57 +0000

    add image optimizer to gulp workflow

--- _src/gulpfile.js
+++ _src/gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
+
+// Styles
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var prefix = require('gulp-autoprefixer');
 var sass = require('gulp-ruby-sass');
+
+// Images
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
 
 var paths = {
   scripts: [''],
   images: ['../imgs/*','../imgs/work/*',],
   styles: ['*.scss', 'sass_imports/*.scss']
 };
+
+
+
 
 gulp.task('sass', function () {
     gulp.src(paths.styles)
@@ -17,6 +26,16 @@ gulp.task('sass', function () {
         .pipe(minifyCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('../'));
+});
+
+gulp.task('images', function () {
+    return gulp.src(paths.images)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
